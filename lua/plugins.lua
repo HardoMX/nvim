c4c2959af5948353f1f83b03d0885ceeab56cdd6
HardoMX <hardo314@gmail.com>
2023-05-24 11:22:37 +0200

    Improve consistant syntax, remove unused file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,44 +12,44 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'AlexvZyl/nordic.nvim'
-  use { 'catppuccin/nvim", as = "catppuccin' }
-  use 'rebelot/kanagawa.nvim'
-  use 'jacoborus/tender.vim'
-  use {'nyoom-engineering/oxocarbon.nvim'}
-  use 'rmehri01/onenord.nvim'
-  use 'folke/tokyonight.nvim'
-  use 'nvim-tree/nvim-tree.lua'
-  use 'nvim-tree/nvim-web-devicons'
-  use 'nvim-lualine/lualine.nvim'
-  use {'kdheepak/tabline.nvim', 
+    use 'wbthomason/packer.nvim'
+    use 'AlexvZyl/nordic.nvim'
+    use { 'catppuccin/nvim", as = "catppuccin' }
+    use 'rebelot/kanagawa.nvim'
+    use 'jacoborus/tender.vim'
+    use {'nyoom-engineering/oxocarbon.nvim'}
+    use 'rmehri01/onenord.nvim'
+    use 'folke/tokyonight.nvim'
+    use 'nvim-tree/nvim-tree.lua'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'nvim-lualine/lualine.nvim'
+    use {'kdheepak/tabline.nvim', 
         requires = { { 'hoob3rt/lualine.nvim', opt=true }, {'kyazdani42/nvim-web-devicons', opt = true} }
     }
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-treesitter/nvim-treesitter-context'
-  use 'm4xshen/autoclose.nvim'
-  use 'jghauser/mkdir.nvim'
-  use 'shellRaining/hlchunk.nvim'
-  use 'NvChad/nvim-colorizer.lua'
-  use 'sitiom/nvim-numbertoggle'
-  use 'RRethy/vim-illuminate'
-  use { 'nvim-telescope/telescope.nvim',
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-treesitter/nvim-treesitter-context'
+    use 'm4xshen/autoclose.nvim'
+    use 'jghauser/mkdir.nvim'
+    use 'shellRaining/hlchunk.nvim'
+    use 'NvChad/nvim-colorizer.lua'
+    use 'sitiom/nvim-numbertoggle'
+    use 'RRethy/vim-illuminate'
+    use { 'nvim-telescope/telescope.nvim',
         tag = '0.1.1',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-  use { 'bennypowers/nvim-regexplainer',
+    use { 'bennypowers/nvim-regexplainer',
         config = function() require'regexplainer'.setup() end,
         requires = {
             'nvim-treesitter/nvim-treesitter',
             'MunifTanjim/nui.nvim',
         } 
     }
-  use 'gelguy/wilder.nvim'
-  use 'folke/which-key.nvim'
-  use 'numToStr/FTerm.nvim'
-  use 'moll/vim-bbye'
-  use { 'folke/noice.nvim',
+    use 'gelguy/wilder.nvim'
+    use 'folke/which-key.nvim'
+    use 'numToStr/FTerm.nvim'
+    use 'moll/vim-bbye'
+    use { 'folke/noice.nvim',
         requires = {
             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
@@ -58,10 +58,13 @@ return require('packer').startup(function(use)
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
         }
-  }
-  use { 'abecodes/tabout.nvim',
+    }
+    use { 'abecodes/tabout.nvim',
         wants = {'nvim-treesitter'}, 
     }
+    use 'ms-jpq/coq_nvim'
+    use 'ms-jpq/coq.artifacts'
+    use 'ms-jpq/coq.thirdparty'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
