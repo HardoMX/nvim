81502584ebf7c05d7f2b09c38fef47643af7cedb
Hardo MX <hardo314@gmail.com>
2023-11-20 17:42:36 +0100

    Continue wilder

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -3,6 +3,19 @@ return {
     config = function ()
         local wilder = require('wilder')
 
-        wilder.setup({modes = {':', '/', '?'}})
+        wilder.setup({ modes = {':', '/', '?'} })
+
+        wilder.set_option('renderer', wilder.popupmenu_renderer(
+            wilder.popupmenu_border_theme({
+                highlights = {
+                    border = 'Normal'
+                },
+                border = 'rounded',
+                highlighter = wilder.basic_highlighter(),
+                left = {' ', wilder.popupmenu_devicons()},
+                right = {' ', wilder.popupmenu_scrollbar()},
+                pumblend = 10,
+            })
+        ))
     end
 }
