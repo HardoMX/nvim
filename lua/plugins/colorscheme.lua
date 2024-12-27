b0db5048c662e9be567075f7027a0edb4b8808c9
Andreas <andreas@andreashard.tech>
2024-12-27 22:25:13 +0100

    Add more colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,78 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "sho-87/kanagawa-paper.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "kepano/flexoki",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "nyoom-engineering/nyoom.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "jacoborus/tender.vim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "olimorris/onedarkpro.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "zenbones-theme/zenbones.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "AlexvZyl/nordic.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "savq/melange-nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "rmehri01/onenord.nvim",
+        lazy = false,
+        priority = 1000,
+    },
 }
