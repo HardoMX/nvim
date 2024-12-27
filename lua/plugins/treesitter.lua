aad2e4501f4184730489762601154398c4f5b096
Andreas <andreas@andreashard.tech>
2024-12-27 20:21:21 +0100

    Add treesitter incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,11 +2,29 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = { "c", "cpp", "lua", "bash", "vim", "vimdoc", "python", "markdown", "markdown_inline", "html", "css", "scss", "javascript", "typescript", "json", "yaml", "yuck", "regex", "go", "toml", "xml", "dockerfile", "sql" },
-            hightlight = { enable = true },
-            indent = { enable = true },
-        }
+        keys = {
+            { "gnn", desc = "Init selection" },
+            { "grn", desc = "Increment node" },
+            { "grc", desc = "Increment scope" },
+            { "grm", desc = "Decrement node" },
+        },
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "cpp", "lua", "bash", "vim", "vimdoc", "python", "markdown", "markdown_inline", "html", "css", "scss", "javascript", "typescript", "json", "yaml", "yuck", "regex", "go", "toml", "xml", "dockerfile", "sql" },
+                hightlight = { enable = true },
+                indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<c-space>",
+                        node_incremental = "<c-space>",
+                        scope_incremental = "gsi",
+                        node_decremental = "<bs>",
+                    },
+                },
+            })
+        end
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
