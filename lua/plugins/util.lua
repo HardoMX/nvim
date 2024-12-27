f59659b8dcc95a2802b619bba595bad0a78d33ab
Andreas <andreas@andreashard.tech>
2024-12-27 21:39:43 +0100

    Add better <C-a> and <C-x> incrementation

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -23,5 +23,23 @@ return {
     },
     {
         "mbbill/undotree",
+    },
+    {
+        "monaqa/dial.nvim",
+        config = function()
+            local augend = require("dial.augend")
+            require("dial.config").augends:register_group {
+                default = {
+                    augend.integer.alias.decimal,
+                    augend.integer.alias.hex,
+                    augend.constant.alias.bool,
+                    augend.date.alias["%Y-%m-%d"],
+                    augend.date.alias["%Y/%m/%d"],
+                    augend.date.alias["%m/%d"],
+                    augend.date.alias["%d/%m"],
+                    augend.date.alias["%H:%M"],
+                }
+            }
+        end
     }
 }
