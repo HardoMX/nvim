b4249aecef3b5dc8b225c2361fe947b105714ab3
Andreas <andreas@andreashard.tech>
2025-02-01 10:51:40 +0100

    Add check to autoformat
    Add if statement to see if current file is the keymap file from qmk, if so, don't autoformat

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -18,13 +18,15 @@ return {
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
                     if not client then return end
 
-                    if client.supports_method("textDocument/formatting", 0) then
-                        vim.api.nvim_create_autocmd("BufWritePre", {
-                            buffer = args.buf,
-                            callback = function()
-                                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-                            end
-                        })
+                    if vim.fn.expand('%') ~= "keymap.c" then -- Remove autoformatting for qmk-keymap file
+                        if client.supports_method("textDocument/formatting", 0) then
+                            vim.api.nvim_create_autocmd("BufWritePre", {
+                                buffer = args.buf,
+                                callback = function()
+                                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                                end
+                            })
+                        end
                     end
                 end,
             })
