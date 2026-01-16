20c42579fbee5b34d9ff83bc006ee5fb7db5c73e
HardoMX <andreas@andreashard.tech>
2026-01-16 15:33:58 +0100

    Remove mason-lspconfig
    Since mason-lspconfig is only used for installing LSPs automatically, it can be removed and done manually, through an install script, or Nix. This allows easier use of this config within containers and NixOS

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -12,32 +12,6 @@ return {
             }
         },
     },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        -- lazy = true,
-        opts = {
-            -- Change these as you like. I do a lot of varied stuff, so have a lot of "necessary" LSPs.
-            -- These are automatically enabled, thus enabling through config/lsp.lua is not strictly necessary
-            ensure_installed = {
-                -- "Necessary" languages
-                "lua_ls",
-                "bashls",
-                "clangd",
-                "pylsp",
-                "vimls",
-                "gopls",
-                "html",
-                "cssls",
-                "yamlls",
-                -- Extra languages (Change as needed)
-                "ansiblels",
-                "terraformls",
-                "marksman",
-                "css_variables",
-                "cssmodules_ls",
-            }
-        },
-    },
     --[[{
         "jay-babu/mason-nvim-dap.nvim",
         -- lazy = true,
